c31e001b1929a8104e2314740b7b4427ddae1e8f
Swetha-C-2004 <swethasms2021@gmail.com>
2025-02-02 23:25:26 +0530

    data store the local storage

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const data=[
+    {
+    id:1,
+    taskName:"reading",
+    status:"completed",
+},
+{
+    id:2,
+    taskName:"Writing",
+    status:"completed",
+
+},
+];
+console.log(JSON.stringify(data));
+function renderTasks(){}
